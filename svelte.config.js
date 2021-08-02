ec012321ac135e55f73ec0823d0911c94a0f8c38
Mike Nikles <mikenikles@pm.me>
2021-08-02 16:46:02 +0000

    Configure Gitpod.

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,15 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443 : 24678,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
+			}
+		}
 	}
 };
 
